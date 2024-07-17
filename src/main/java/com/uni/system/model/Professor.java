6bb565db854bbe21a616a93c73bc2c651425f6e5
0jun01 <dudwns85064843@gmail.com>
2024-07-17 17:41:06 +0900

    complete DTO

--- src/main/java/com/uni/system/model/Professor.java
+++ src/main/java/com/uni/system/model/Professor.java
@@ -2,6 +2,17 @@ package com.uni.system.model;
 
 import java.sql.Date;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
 public class Professor {
 	
 	int id;
