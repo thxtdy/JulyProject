846fbb24f7e01f6333cb56123866dfa1ce52b700
thxtdy <thjjang0409@gmail.com>
2024-07-17 17:32:04 +0900

    tableDTO

---
+++ src/main/java/com/uni/system/model/BreakApp.java
@@ -0,0 +1,28 @@
+package com.uni.system.model;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class BreakApp {
+	
+	private int id;
+	private int studentId;
+	private int studentGrade;
+	private int fromYear;
+	private int fromSemester;
+	private int toYear;
+	private int toSemester;
+	private String type;
+	private Date appDate;
+	private String status;
+	
+}
