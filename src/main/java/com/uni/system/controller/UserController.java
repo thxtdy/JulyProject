261116d539290b2bcb354cba14cd9b6d457ed1d6
thxtdy <thjjang0409@gmail.com>
2024-07-22 22:42:34 +0900

    LogOut Path Edit

--- src/main/java/com/uni/system/controller/UserController.java
+++ src/main/java/com/uni/system/controller/UserController.java
@@ -52,7 +52,7 @@ public class UserController extends HttpServlet {
 		HttpSession session = request.getSession();
 		session.invalidate();
 
-		response.sendRedirect(request.getContextPath() + "/user/index");
+		response.sendRedirect(request.getContextPath() + "/index.jsp");
 
 	}
 
